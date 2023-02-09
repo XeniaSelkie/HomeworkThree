d2311e409f13b3aa70a667b7b4487f8add025296
Sana <mypostoffice94@gmail.com>
2023-02-09 16:12:11 +0300

    Add Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,28 @@
+﻿int fiveDigitNumb = ReadInt("пятизначное число");
+
+int ReadInt(string argument)
+{
+	Console.Write($"Введите {argument}: ");
+	int number;
+
+	while (!int.TryParse(Console.ReadLine(), out number))
+	{
+		Console.WriteLine("Ошибка ввода, пожалуйста, введите корректное значение");
+	}
+
+	return number;
+}
+
+if (fiveDigitNumb > 9999 && fiveDigitNumb < 100000)
+{
+    string palindtromeDigit = fiveDigitNumb.ToString();
+    if (palindtromeDigit[0] == palindtromeDigit[4] && palindtromeDigit[1] == palindtromeDigit[3])
+    {
+        Console.WriteLine($"{fiveDigitNumb} является палиндромом");
+    }
+    else Console.WriteLine($"{fiveDigitNumb} НЕ является палиндромом");
+}
+else
+{
+    Console.WriteLine("Ошибка ввода, пожалуйста, введите корректное значение");
+}
